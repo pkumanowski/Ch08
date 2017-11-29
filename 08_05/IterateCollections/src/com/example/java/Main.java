240e42f4a86f147f5e8b4f39b099412458aa61e6
pkumanowski <pkumanowski@outlook.com>
2017-11-29 21:18:57 +0100

    iterateCollections

--- 08_05/IterateCollections/src/com/example/java/Main.java
+++ 08_05/IterateCollections/src/com/example/java/Main.java
@@ -21,28 +21,46 @@ public class Main {
         System.out.println(list);
         System.out.println("");
 
-//        System.out.println("ArrayList iterator");
-//        System.out.println("");
-//
-//        System.out.println("ArrayList ForEach");
-//        System.out.println("");
-//
-//        System.out.println("Java 8 method reference");
-//        System.out.println("");
-//
-//        System.out.println("Unordered data");
-//        HashMap<String, String> map = new HashMap<String, String>();
-//        map.put("California", "Sacramento");
-//        map.put("Oregon", "Salem");
-//        map.put("Washington", "Olympia");
-//        System.out.println(map);
-//        System.out.println("");
-//
-//        System.out.println("HashMap Iterator");
-//        System.out.println("");
-//
-//        System.out.println("HashMap ForEach");
-//        System.out.println("");
+        System.out.println("ArrayList iterator");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String value = iterator.next();
+            System.out.println(value);
+        }
+        System.out.println("");
+
+        System.out.println("ArrayList ForEach");
+        for (String value : list){
+            System.out.println(value);
+        }
+        System.out.println("");
+
+        System.out.println("Java 8 method reference");
+        list.forEach(System.out::println);
+        System.out.println("");
+
+        System.out.println("Unordered data");
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("California", "Sacramento");
+        map.put("Oregon", "Salem");
+        map.put("Washington", "Olympia");
+        System.out.println(map);
+        System.out.println("");
+
+        System.out.println("HashMap Iterator");
+        Set<String> keys = map.keySet();
+        Iterator<String> iterator1 = keys.iterator();
+        while (iterator1.hasNext()){
+            String key = iterator1.next();
+            System.out.println("Capitol of " + key + " is " + map.get(key));
+        }
+        System.out.println("");
+
+        System.out.println("HashMap ForEach");
+        for (String key : keys){
+            System.out.println("Capitol of " + key + " is " + map.get(key));
+        }
+        System.out.println("");
 
     }
 
